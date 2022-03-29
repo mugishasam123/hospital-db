6d46064e7854ef42da5884e9108458f70cbbd3b1
mugisha Samuel <mugishasamuel42@gmail.com>
2022-03-29 02:51:38 -0700

    Add database queries

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE hospitaldb;
+
+
+CREATE TABLE patients(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+name VARCHAR(255),
+date_of_birth DATE);
+
+
+CREATE TABLE medical_histories(id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+admitted_at TIMESTAMP,
+patient_id INT,
+status VARCHAR(255),
+FOREIGN KEY(patient_id) REFERENCES patients(id));
+
+
+CREATE TABLE invoices(id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+total_amount DECIMAL,
+generated_at TIMESTAMP,
+payed_at TIMESTAMP,
+medical_history_id INT,
+FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id));
+
+
+CREATE TABLE treatments(id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+type VARCHAR(255),
+name VARCHAR(255));
+
+
+CREATE TABLE invoice_items(id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+unit_price DECIMAL,
+quantity INT,
+total_price DECIMAL,
+invoice_id INT,
+treatment_id INT,
+FOREIGN KEY(invoice_id) REFERENCES invoices(id),FOREIGN KEY(treatment_id) REFERENCES treatments(id));
+
